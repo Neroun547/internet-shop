a6254f6370c3ba35314c5d1f83ed2e2f42063c44
Gosha Kovalenko <kovalenko@giria.io>
2024-02-11 15:29:49 +0100

    Added some animation for show left bar menu btn

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,16 @@
 const wrapperMenuBtn = document.querySelector(".wrapper__logo-open-menu");
 const wrapperContentLeftBarMenu = document.querySelector(".wrapper__content-left-bar-menu");
 const languageSelect = document.getElementById("language-select");
+const wrapperLogoMenuDecoration1 = wrapperMenuBtn.querySelector(".wrapper__logo-open-menu-decoration-1");
+const wrapperLogoMenuDecoration2 = wrapperMenuBtn.querySelector(".wrapper__logo-open-menu-decoration-2");
+const wrapperLogoMenuDecoration3 = wrapperMenuBtn.querySelector(".wrapper__logo-open-menu-decoration-3");
 
 let showMenu = false;
 let moveForMenu = wrapperContentLeftBarMenu.clientWidth / 8;
+
+if(!window.matchMedia("(max-width: 1530px)").matches) {
+    showCrossLeftBarMenuBtnDecoration();
+}
 
 wrapperMenuBtn.addEventListener("click",  () => {
 
@@ -18,6 +25,8 @@ wrapperMenuBtn.addEventListener("click",  () => {
     if(!showMenu) {
         let left = 0;
 
+        hideCrossLeftBarMenuBtnDecoration();
+
         const interval = setInterval(() => {
             if (left > (-wrapperContentLeftBarMenu.clientWidth)) {
                 left -= moveForMenu;
@@ -29,6 +38,10 @@ wrapperMenuBtn.addEventListener("click",  () => {
         }, 10);
     } else {
         let left = (-wrapperContentLeftBarMenu.clientWidth);
+
+
+        showCrossLeftBarMenuBtnDecoration();
+
         const interval = setInterval(() => {
             if (left < 0) {
                 left += moveForMenu;
@@ -49,6 +62,32 @@ languageSelect.addEventListener("change", async function (e) {
 
     window.location.reload();
 });
+
+function showCrossLeftBarMenuBtnDecoration() {
+    wrapperLogoMenuDecoration1.style.position = "absolute";
+    wrapperLogoMenuDecoration1.style.top = "24px";
+
+    wrapperLogoMenuDecoration1.style.rotate = "45deg";
+
+    wrapperLogoMenuDecoration2.style.opacity = "0";
+
+    wrapperLogoMenuDecoration3.style.position = "absolute";
+    wrapperLogoMenuDecoration3.style.top = "15px";
+    wrapperLogoMenuDecoration3.style.rotate = "-45deg";
+}
+
+function hideCrossLeftBarMenuBtnDecoration() {
+    wrapperLogoMenuDecoration1.style.position = "static";
+    wrapperLogoMenuDecoration1.style.top = "0";
+
+    wrapperLogoMenuDecoration1.style.rotate = "0deg";
+
+    wrapperLogoMenuDecoration2.style.opacity = "1";
+
+    wrapperLogoMenuDecoration3.style.position = "static";
+    wrapperLogoMenuDecoration3.style.top = "0";
+    wrapperLogoMenuDecoration3.style.rotate = "0deg";
+}
 
 
 
