39d6b5aad0b21f46cc9e21748b28d0bde175af47
Gosha <kovalenko@giria.io>
2024-09-08 15:22:28 +0200

    Changed positions some middlewares

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,6 @@ async function bootstrap() {
 
   app.useStaticAssets(resolve( "static"));
 
-  app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({limit: '50mb', extended: true}));
-  app.use(cookieParser());
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  app.use('/tinymce', express.static(join(__dirname, 'node_modules', 'tinymce')));
-
   app.setBaseViewsDir(resolve( "views"));
   app.setViewEngine("hbs");
 
@@ -39,6 +31,14 @@ async function bootstrap() {
       }
     }
   }));
+
+  app.use('/tinymce', express.static(join(__dirname, 'node_modules', 'tinymce')));
+
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({limit: '50mb', extended: true}));
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
